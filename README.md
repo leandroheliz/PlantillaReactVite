0cf4e647822de9426ae4ddba91b3cc39f6b9194e
Leandro Heliz <leandroheliz@gmail.com>
2022-05-04 14:50:13 -0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+## Cree esta plantilla con el fin de agili
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom"
+import RouterApp from "./routes/RouterApp"
+
+function App() {
+ 
+  return (
+    <>
+    <BrowserRouter>
+    <RouterApp />
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
